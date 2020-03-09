0ac8b3b23beb7cbf40354036d0ec5ce47ffbcec2
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-09 15:56:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,3 @@
-
-
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
@@ -8,4 +6,9 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
 end
